c10ed81edf7e19d86d616c75d1f698db98d054da
Suvam Das <dassuvam420@gmail.com>
2021-08-09 16:22:48 +0530

    Second Commit

--- src/main/java/com/api/employee/EmployeeManagementApplication.java
+++ src/main/java/com/api/employee/EmployeeManagementApplication.java
@@ -3,11 +3,17 @@ package com.api.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+//Causes Lombok to generate a logger field.
+@Slf4j
+//Main implementation class which serves two purposes in a spring boot application: Configuration and bootstrapping.
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
+		
+		log.info("Server Started Successfully");
 	}
 
 }
--- src/main/java/com/api/employee/entities/Employee.java
+++ src/main/java/com/api/employee/entities/Employee.java
@@ -7,7 +7,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//Causes lombok to generate toString(), equals(), hashCode(), getter() & setter(), and Required arguments constructor in one go.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Employee implements Serializable{
 
@@ -22,46 +28,5 @@ public class Employee implements Serializable{
 	private String empName;
 	private String empMobileNumber;
 	private String empAddress;
-	public Employee(Long id, String empId, String empName, String empMobileNumber, String empAddress) {
-		super();
-		this.id = id;
-		this.empId = empId;
-		this.empName = empName;
-		this.empMobileNumber = empMobileNumber;
-		this.empAddress = empAddress;
-	}
-	public Employee() {
-		super();
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getEmpId() {
-		return empId;
-	}
-	public void setEmpId(String empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public String getEmpMobileNumber() {
-		return empMobileNumber;
-	}
-	public void setEmpMobileNumber(String empMobileNumber) {
-		this.empMobileNumber = empMobileNumber;
-	}
-	public String getEmpAddress() {
-		return empAddress;
-	}
-	public void setEmpAddress(String empAddress) {
-		this.empAddress = empAddress;
-	}
 
 }
